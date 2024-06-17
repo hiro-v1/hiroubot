de9d67160ea6991181c6ca38778fc027b049489c
Dante <148047531+Vooyage21@users.noreply.github.com>
2024-06-18 04:09:06 +0700

    Update misc.py

--- DanteUserbot/core/helpers/misc.py
+++ DanteUserbot/core/helpers/misc.py
@@ -179,7 +179,7 @@ def paginate_modules(page_n, module_dict, prefix, chat=None):
                     callback_data="{}_prev({})".format(prefix, modulo_page),
                 ),
                 EqInlineKeyboardButton(
-                    "ᴍᴇɴᴜ",
+                    "⌂",
                     callback_data="cl_ad",
                 ),
                 EqInlineKeyboardButton(
