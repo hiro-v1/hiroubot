901e1d050cde3baaf35c36edae23fc12dd778c00
Dante <148047531+Vooyage21@users.noreply.github.com>
2024-07-01 06:12:44 +0700

    Update topcmd.py

--- DanteUserbot/modules/topcmd.py
+++ DanteUserbot/modules/topcmd.py
@@ -2,7 +2,6 @@ from DanteUserbot import *
 
 
 @DANTE.UBOT("top")
-@DANTE.ADMIN
 async def _(client, message):
     vars = await all_vars(bot.me.id, "modules")
     sorted_vars = sorted(vars.items(), key=lambda item: item[1], reverse=True)
