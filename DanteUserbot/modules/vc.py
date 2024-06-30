1f95071c923636b73fd7ff5f730fafbdd7dd7407
Dante <148047531+Vooyage21@users.noreply.github.com>
2024-07-01 05:21:13 +0700

    Update vc.py

--- DanteUserbot/modules/vc.py
+++ DanteUserbot/modules/vc.py
@@ -123,6 +123,7 @@ async def leavevc(client, message):
         try:
             await client.call_py.leave_call(chat_id)
             await message.reply(f"**❏ Berhasil Meninggalkan Voice Chat <emoji id=5798623990436074786>✅</emoji>**\n")
+            await sleep(1)
             await message.delete()
         except Exception as e:
             return await message.reply_text(e)
