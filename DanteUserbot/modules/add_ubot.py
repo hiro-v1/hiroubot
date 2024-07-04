5a306ce361d1e27d11e360bd90aea465e3cc2f91
Dante <148047531+Vooyage21@users.noreply.github.com>
2024-07-04 18:32:58 +0700

    Update add_ubot.py

--- DanteUserbot/modules/add_ubot.py
+++ DanteUserbot/modules/add_ubot.py
@@ -258,7 +258,6 @@ async def bikin_ubot(client, callback_query):
     text_done = f"<b>💠 {bot.me.mention} berhasil diaktifkan di akun: <a href=tg://openmessage?user_id={new_client.me.id}>{new_client.me.first_name} {new_client.me.last_name or ''}</a> > <code>{new_client.me.id}</code></b> "
     await bot_msg.edit(text_done)
     try:
-        await new_client.join_chat("StreamTelegram_Id")
         await new_client.join_chat("TemanDemus_Id")
     except UseralreadyParticipant:
         pass
