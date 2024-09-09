98b42740e8d3236e043db85d7432528fab9c15c6
Dante <148047531+Vooyage21@users.noreply.github.com>
2024-09-09 09:59:17 +0700

    Update hapuscht.py

--- DanteUserbot/modules/hapuscht.py
+++ DanteUserbot/modules/hapuscht.py
@@ -11,27 +11,60 @@ __HELP__ = """<blockquote><b>
   <b>• Example:</b> hapus seluruh chat kamu.
   </b></blockquote> """
 
-# by dante
-# perintah untuk menghapus semua pesan
-@DANTE.UBOT("clearall")
-async def clearchat(client, message):
-    chat_id = message.chat.id
+@DANTE.UBOT("cc")
+async def _(c: nlx, m, _):
+    org = await c.extract_user(m)
+    if not org:
+        a = await m.reply(_("prof_1").format(em.gagal))
+        await asyncio.sleep(2)
+        return await a.delete()
+    user = await c.get_users(org)
+    await m.delete()
+    return await c.delete_user_history(m.chat.id, user.id)
 
-    # send msg to show that bot is working
-    await message.edit("Sedang menghapus semua chat anda tunggu...")
-    await asyncio.sleep(2)
-
-    # get all messages
-    async for message in client.get_chat_history(chat_id):
-        if not message.message_id:
-            break
+@DANTE.UBOT("cc")
+async def _(c: nlx, m, _):
+    em = Emojik(c)
+    em.initialize()
+    rep = m.reply_to_message
+    mek = await m.reply("proses")
+    if len(m.command) < 2 and not rep:
+        await m.reply(_("").format(em.gagal))
+        return
+    if len(m.command) == 1 and rep:
+        who = rep.from_user.id
         try:
-            # delete message
-            await client.delete_user_history(chat_id, message.from_user.id)
-        except FloodWait as e:
-            # wait for a while
-            print(e)
-            await asyncio.sleep(e.x)
-        except Exception as e:
-            print(e)
+            info = await c.resolve_peer(who)
+            await c.invoke(DeleteHistory(peer=info, max_id=0, revoke=True))
+        except PeerIdInvalid:
             pass
+        await m.reply(_("auend_2").format(em.sukses, who))
+    else:
+        if m.command[1].strip().lower() == "all":
+            biji = await c.get_chats_dialog("usbot")
+            for kelot in biji:
+                try:
+                    info = await c.resolve_peer(kelot)
+                    await c.invoke(DeleteHistory(peer=info, max_id=0, revoke=True))
+                except PeerIdInvalid:
+                    continue
+            await m.reply(_("auend_3").format(em.sukses, len(biji)))
+        elif m.command[1].strip().lower() == "bot":
+            bijo = await c.get_chats_dialog("bot")
+            for kelot in bijo:
+                try:
+                    info = await c.resolve_peer(kelot)
+                    await c.invoke(DeleteHistory(peer=info, max_id=0, revoke=True))
+                except PeerIdInvalid:
+                    continue
+            await m.reply(_("auend_4").format(em.sukses, len(bijo)))
+        else:
+            who = m.text.split(None, 1)[1]
+            try:
+                info = await c.resolve_peer(who)
+                await c.invoke(DeleteHistory(peer=info, max_id=0, revoke=True))
+            except PeerIdInvalid:
+                pass
+            await m.reply(_("auend_2").format(em.sukses, who))
+    await mek.delete()
+    return
