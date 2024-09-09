a6ca00575b4b745f9f2947e9f873ca3035944b4c
Dante <148047531+Vooyage21@users.noreply.github.com>
2024-09-09 14:03:19 +0700

    Update hapuscht.py

--- DanteUserbot/modules/hapuscht.py
+++ DanteUserbot/modules/hapuscht.py
@@ -1,71 +1,26 @@
 from pyrogram import Client, filters
-from pyrogram.errors import FloodWait
 import asyncio
-from pyrogram.errors import *
-from pyrogram.raw.functions.messages import DeleteHistory
-from DanteUserbot import *
+import time
+from pyrogram.types import ChatPermissions, Message
 
-__MODULE__ = "ᴅᴇʟᴄʜᴀᴛ"
-__HELP__ = """<blockquote><b>
-<b>『 Clear semua chat 』</b>
+@DANTE.UBOT(ccall)
+async def delete_user_history(client: Client, message: Message):
+    chat_id=message.chat.id
+    msg_id=message.message_id
+    chat_msg=message.text
+    username=None
 
-  <b>• delchat:</b> <code>{0}delchat</code></code>
-  <b>• Example:</b> hapus seluruh chat kamu.
-  </b></blockquote> """
-
-@DANTE.UBOT("cc")
-async def cc(client: Client, message: Message):
-    org = await client.extract_user(message)
-    if not org:
-        a = await message.reply.text("proses")
-        await asyncio.sleep(2)
-        return await a.delete()
-    user = await client.get_users(org)
-    await message.delete()
-    return await client.delete_user_history(message.chat.id, user.id)
-
-
-@DANTE.UBOT("clchat")
-async def clchat(client: Client, message: Message):
-    rep = message.reply_to_message
-    mek = await message.reply.text("proses")
-    if len(message.command) < 2 and not rep:
-        await message.reply.text("gagal menghapus chat")
-        return
-    if len(message.command) == 1 and rep:
-        who = rep.from_user.id
-        try:
-            info = await client.resolve_peer(who)
-            await client.invoke(DeleteHistory(peer=info, max_id=0, revoke=True))
-        except PeerIdInvalid:
-            pass
-        await message.reply("berhasil menghapus seluruh chat kamu")
+    if "@" in chat_msg:
+        index=chat_msg.index("@")     
+        chat_msg=str(chat_msg)
+        username=chat_msg[index+1:len(chat_msg)]
+    else:                   
+        username=message.reply_to_message.from_user.id
+    zuzu= await client.get_chat_member(chat_id , "me")
+    can_delete=zuzu.can_delete_messages
+    if(can_delete):      
+        await client.delete_user_history(chat_id , username)
     else:
-        if message.command[1].strip().lower() == "all":
-            biji = await client.get_chats_dialog("usbot")
-            for kelot in biji:
-                try:
-                    info = await client.resolve_peer(kelot)
-                    await message.invoke(DeleteHistory(peer=info, max_id=0, revoke=True))
-                except PeerIdInvalid:
-                    continue
-            await message.reply.text("sukses").format(len(biji))
-        elif message.command[1].strip().lower() == "bot":
-            bijo = await client.get_chats_dialog("bot")
-            for kelot in bijo:
-                try:
-                    info = await client.resolve_peer(kelot)
-                    await client.invoke(DeleteHistory(peer=info, max_id=0, revoke=True))
-                except PeerIdInvalid:
-                    continue
-            await message.reply.text("sukses").format(bijo)
-        else:
-            who = message.text.split(None, 1)[1]
-            try:
-                info = await client.resolve_peer(who)
-                await c.invoke(DeleteHistory(peer=info, max_id=0, revoke=True))
-            except PeerIdInvalid:
-                pass
-            await message.reply.text("berhasil").format(who)
-    await mek.delete()
-    return
+        reply_string="Noob, kamu tidak bisa menghapus keberadaan mereka !"
+        await client.edit_message_text(chat_id , msg_id , reply_string )
+      
