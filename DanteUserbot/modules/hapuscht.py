c26d2f65c2b66adbb7e5076de6e81abc1a6b5df5
Dante <148047531+Vooyage21@users.noreply.github.com>
2024-09-09 09:48:59 +0700

    Update hapuscht.py

--- DanteUserbot/modules/hapuscht.py
+++ DanteUserbot/modules/hapuscht.py
@@ -18,16 +18,16 @@ async def clearchat(client, message):
     chat_id = message.chat.id
 
     # send msg to show that bot is working
-    await message.edit("Sedang menghapus tunggu...")
+    await message.edit("Sedang menghapus semua chat anda tunggu...")
     await asyncio.sleep(2)
 
     # get all messages
-    async for msg in client.get_chat_history(chat_id):
-        if not msg.message_id:
+    async for message in client.get_chat_history(chat_id):
+        if not message.message_id:
             break
         try:
             # delete message
-            await client.delete_user_history(chat_id, msg.from_user.id)
+            await client.delete_user_history(chat_id, message.from_user.id)
         except FloodWait as e:
             # wait for a while
             print(e)
