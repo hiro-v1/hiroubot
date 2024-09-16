7d64be94563cc67eaa788e8b9ca719642af0ade3
Dante <148047531+Vooyage21@users.noreply.github.com>
2024-09-16 12:55:18 +0700

    Update read.py

--- DanteUserbot/modules/read.py
+++ DanteUserbot/modules/read.py
@@ -27,15 +27,3 @@ async def baca_read(client, message):
                 done += 1
     await Mai.edit_text(f"Berhasil Membaca Pesan Dari : {done} User✅")
 
-@DANTE.UBOT("hps")
-async def delmessage(client, message):
-    Mai = await message.reply_text(f"Proccesing...")
-    done = 0
-    async for dialog in client.get_dialogs():
-        if dialog.chat.type == ChatType.PRIVATE:
-            user_id = dialog.chat.id
-            anjai = await client.DeleteHistory(user_id)
-            if anjai:
-                done += 1
-    await Mai.edit_text(f"Berhasil menghapus Pesan Dari : {done} User✅")
-  
