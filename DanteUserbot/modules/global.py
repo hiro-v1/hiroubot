a87e94852fea44e3d8008025eff0e90df9e5200e
Dante <148047531+Vooyage21@users.noreply.github.com>
2024-09-21 21:35:07 +0700

    Update global.py

--- DanteUserbot/modules/global.py
+++ DanteUserbot/modules/global.py
@@ -12,6 +12,12 @@ __HELP__ = f"""<blockquote><b>
 
   <b>• ᴄᴏᴍᴍᴀɴᴅ:</b> <code>{PREFIX[0]}listgban</code>
   <b>• ᴇxᴘʟᴀɴᴀᴛɪᴏɴ:</b> ᴜɴᴛᴜᴋ ᴍᴇʟɪʜᴀᴛ ᴅᴀꜰᴛᴀʀ ᴘᴇɴɢɢᴜɴᴀ ɢʙᴀɴ.
+
+  <b>• perintah : <code>{0}gmute</code>
+    untuk mengemute user dari semua gc yg kmu admin 
+    
+  <b>• perintah : <code>{0}ungmute</code>
+    untuk meng ungmute user dari semua gc yg kmu admin
 </b></blockquote>"""
 import asyncio
 
@@ -181,6 +187,79 @@ async def gbanlist(client, message):
     else:
         return await nyet.edit(msg)
 
+@DANTE.UBOT("gmute")
+async def _(client, message):
+    sks = await EMO.BERHASIL(client)
+    prs = await EMO.PROSES(client)
+    ggl = await EMO.GAGAL(client)
+    user_id = await extract_user(message)
+    _msg = f"{prs}proceꜱꜱing..."
+
+    Tm = await message.reply(_msg)
+    if not user_id:
+        return await Tm.edit(f"{ggl}user tidak ditemukan")
+    try:
+        user = await client.get_users(user_id)
+    except Exception as error:
+        return await Tm.edit(error)
+    done = 0
+    failed = 0
+    text = "global {}\n\nberhasil: {} chat\ngagal: {} chat\nuser: <a href='tg://user?id={}'>{} {}</a>"
+    global_id = await get_data_id(client, "group")
+    for dialog in global_id:
+        if user.id == OWNER_ID:
+            return await Tm.edit(f"{ggl}anda tidak bisa gmute dia karena dia pembuat saya")
+        try:
+            await client.restrict_chat_member(dialog, user.id, ChatPermissions(can_send_messages=False))
+            done += 1
+            await asyncio.sleep(0.1)
+        except Exception:
+            failed += 1
+            await asyncio.sleep(0.1)
+    await message.reply(
+        text.format(
+            "mute", done, failed, user.id, user.first_name, (user.last_name or "")
+        )
+    )
+    return await Tm.delete()
+
+@DANTE.UBOT("ungmute")
+async def _(client, message):
+    sks = await EMO.BERHASIL(client)
+    prs = await EMO.PROSES(client)
+    ggl = await EMO.GAGAL(client)
+    user_id = await extract_user(message)
+    _msg = f"{prs}proceꜱꜱing..."
+    Tm = await message.reply(_msg)
+    if not user_id:
+        return await Tm.edit(f"{ggl}user tidak ditemukan")
+    try:
+        user = await client.get_users(user_id)
+    except Exception as error:
+        return await Tm.edit(error)
+    done = 0
+    failed = 0
+    text = "global {}\n\nberhasil: {} chat\ngagal: {} chat\nuser: <a href='tg://user?id={}'>{} {}</a>"
+    global_id = await get_data_id(client, "global")
+    for dialog in global_id:
+        try:
+            await client.restrict_chat_member(dialog, user.id, ChatPermissions(can_send_messages=True))
+            done += 1
+            await asyncio.sleep(0.1)
+        except Exception:
+            failed += 1
+            await asyncio.sleep(0.1)
+    await message.reply(
+        text.format(
+            "ungmuted",
+            done,
+            failed,
+            user.id,
+            user.first_name,
+            (user.last_name or ""),
+        )
+    )
+    return await Tm.delete()
 
 @DANTE.UBOT("gban")
 #@ubot.on_message(filters.user(DEVS) & filters.command("cgban", "") & ~filters.me)
