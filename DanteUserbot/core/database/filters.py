bd389185d539f903dfac21c1238d7cfb4057da82
Dante <148047531+Vooyage21@users.noreply.github.com>
2024-09-21 22:34:43 +0700

    Update filters.py

--- DanteUserbot/core/database/filters.py
+++ DanteUserbot/core/database/filters.py
@@ -14,6 +14,37 @@ mongodb = mongo_client.dante_userbot
 db = mongodb.premium
 filtersdb = db.filters
 
+async def get_gmuteh_users(gua: int) -> list:
+    results = []
+    async for user in gmute.find({"gua": gua, "user_id": {"$gt": 0}}):
+        results.append(user["user_id"])
+    return results
+
+
+async def get_gmuteh_count(gua: int) -> int:
+    users = gmute.find({"gua": gua, "user_id": {"$gt": 0}})
+    users = await users.to_list(length=100000)
+    return len(users)
+
+
+async def is_gmuteh_user(gua: int, user_id: int) -> bool:
+    user = await gmute.find_one({"gua": gua, "user_id": user_id})
+    return bool(user)
+
+
+async def add_gmuteh_user(gua: int, user_id: int):
+    is_ggmuteh = await is_gmuteh_user(gua, user_id)
+    if is_ggmuteh:
+        return
+    return await gmute.insert_one({"gua": gua, "user_id": user_id})
+
+
+async def remove_gmuteh_user(gua: int, user_id: int):
+    is_ggmuteh = await is_gmuteh_user(gua, user_id)
+    if not is_ggmuteh:
+        return
+    return await gmute.delete_one({"gua": gua, "user_id": user_id})
+    
 async def get_filters_count() -> dict:
     chats_count = 0
     filters_count = 0
