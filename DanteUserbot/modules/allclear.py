d73ef55f323f77cce2a41c9707b82adae7aedd14
Dante <148047531+Vooyage21@users.noreply.github.com>
2024-09-27 02:17:24 +0700

    Update allclear.py

--- DanteUserbot/modules/allclear.py
+++ DanteUserbot/modules/allclear.py
@@ -40,3 +40,47 @@ async def cc(client, message):
         print(f"Error saat menghapus pesan: {ev}")
         await message.reply("berhasil menghapus semua pesan pengguna.")  # Memberi tahu user      
 
+
+async def (client, message):
+    rep = m.reply_to_message
+    mek = await m.reply(_("proses").format(em.proses))
+    if len(m.command) < 2 and not rep:
+        await m.reply(_("auend_1").format(em.gagal))
+        return
+    if len(m.command) == 1 and rep:
+        who = rep.from_user.id
+        try:
+            info = await c.resolve_peer(who)
+            await c.invoke(DeleteHistory(peer=info, max_id=0, revoke=True))
+        except PeerIdInvalid:
+            pass
+        await m.reply(_("auend_2").format(em.sukses, who))
+    else:
+        if m.command[1].strip().lower() == "all":
+            biji = await c.get_chats_dialog("usbot")
+            for kelot in biji:
+                try:
+                    info = await c.resolve_peer(kelot)
+                    await c.invoke(DeleteHistory(peer=info, max_id=0, revoke=True))
+                except PeerIdInvalid:
+                    continue
+            await m.reply(_("auend_3").format(em.sukses, len(biji)))
+        elif m.command[1].strip().lower() == "bot":
+            bijo = await c.get_chats_dialog("bot")
+            for kelot in bijo:
+                try:
+                    info = await c.resolve_peer(kelot)
+                    await c.invoke(DeleteHistory(peer=info, max_id=0, revoke=True))
+                except PeerIdInvalid:
+                    continue
+            await m.reply(_("auend_4").format(em.sukses, len(bijo)))
+        else:
+            who = m.text.split(None, 1)[1]
+            try:
+                info = await c.resolve_peer(who)
+                await c.invoke(DeleteHistory(peer=info, max_id=0, revoke=True))
+            except PeerIdInvalid:
+                pass
+            await m.reply(_("auend_2").format(em.sukses, who))
+    await mek.delete()
+    return
