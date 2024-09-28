b8aad130472b937e411e8780e07f21559d50317f
Dante <148047531+Vooyage21@users.noreply.github.com>
2024-09-28 23:50:30 +0700

    Update start.py

--- DanteUserbot/modules/start.py
+++ DanteUserbot/modules/start.py
@@ -4,7 +4,6 @@ from datetime import datetime
 import sys
 from gc import get_objects
 from time import time
-from DanteUserbot.core.helpers.EMJI import *
 from DanteUserbot import bot, ubot
 from pyrogram.errors.exceptions.bad_request_400 import UserBannedInChannel
 from pyrogram.raw.functions import Ping
